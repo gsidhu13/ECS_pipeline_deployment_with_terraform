b80df1aca99674b47d5f00b8510bff0da7dc53d2
Gurlal <ggssidhu12@gmail.com>
2021-11-09 23:50:52 -0800

    repo created

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_codecommit_repository" "ecs_repo" {
+
+  repository_name = "pipeline_ecs_repo"
+  description     = "repository for ECS pipeline"
+
+}
