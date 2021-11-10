d4ebb3548d314c49a4e1141bc04a9ffd8a14bc0b
Gurlal <ggssidhu12@gmail.com>
2021-11-10 01:23:51 -0800

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,9 +14,72 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_codecommit_repository" "ecs_repo" {
+#codecommit repository that stores source code
 
-  repository_name = "pipeline_ecs_repo"
+resource "aws_codecommit_repository" "source_repo" {
+
+  repository_name = "pipeline_source_repo"
   description     = "repository for ECS pipeline"
+
+}
+
+#ECR repo to store images for ECS 
+
+resource "aws_ecr_repository" "ecr_repo" {
+  name = "pipeline_ecr_repo"
+}
+
+#taske and execution role for task defination/cluster
+
+resource "aws_iam_role" "task_defination_role" {
+  name        = "task_defination_execution_role"
+  description = "Allows ECS tasks to call AWS services on your behalf"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy", ]
+}
+
+
+#create an ECS cluster for pipeline
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "pipelineCluster"
+
+}
+
+resource "aws_ecs_task_definition" "defination" {
+  family = "pipeline_task"
+  container_definitions = jsonencode([
+    {
+      name   = "webapp"
+      image  = "${aws_ecr_repository.ecr_repo.repository_url}"
+      memory = 256
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+
+  cpu                = 256
+  memory             = 512
+  execution_role_arn = aws_iam_role.task_defination_role.arn
+  network_mode       = "awsvpc"
+  requires_compatibilities = [
+  "FARGATE"]
+  task_role_arn = aws_iam_role.task_defination_role.arn
 
 }
