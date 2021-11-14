a48238135f6a8a8bd7355419544550966cb129d8
Gurlal <ggssidhu12@gmail.com>
2021-11-14 00:25:36 -0800

    update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,6 +2,6 @@ output "ecr_repo_arn" {
   value = aws_ecr_repository.ecr_repo.arn
 }
 output "container_name" {
-value = aws_ecs_task_definition.defination.container_definitions
-  
+  value = aws_ecs_task_definition.defination.container_definitions
+
 }
