e1cba79b9029d950657a9ae0db1e2780f02a742f
Gurlal <ggssidhu12@gmail.com>
2021-11-15 01:49:18 -0800

    final main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,19 +14,9 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-#codecommit repository that stores source code
-
-resource "aws_codecommit_repository" "source_repo" {
-
-  repository_name = var.repo_name
-  description     = "repository for ECS pipeline"
-
-}
-
 #ECR repo to store images for ECS 
-
 resource "aws_ecr_repository" "ecr_repo" {
-  name = "pipeline_ecr_repo"
+  name = var.ecr_repo_name
 }
 
 #taske and execution role for task defination/cluster
@@ -50,11 +40,10 @@ resource "aws_iam_role" "task_defination_role" {
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy", ]
 }
 
-
 #create an ECS cluster for pipeline
 
 resource "aws_ecs_cluster" "cluster" {
-  name = "pipelineCluster"
+  name = var.cluster_name
 
 }
 #create task defination 
@@ -82,8 +71,7 @@ resource "aws_ecs_task_definition" "defination" {
   task_role_arn = aws_iam_role.task_defination_role.arn
 
 }
-#grab default vpc and create security group 
-
+#grab default vpc and create security group for this cluster
 resource "aws_default_vpc" "default" {
   tags = {
     Name = "Default VPC"
@@ -162,6 +150,9 @@ resource "aws_iam_role" "codepipeline_service_role" {
     ]
   })
 }
+
+#policy for codepipeline service role
+
 resource "aws_iam_role_policy" "pipeline_polciy" {
   role = aws_iam_role.codepipeline_service_role.id
   policy = jsonencode({
@@ -343,38 +334,157 @@ resource "aws_iam_role_policy" "pipeline_polciy" {
 
 #codebuild role for ECS
 
-# resource "aws_iam_role" "codebuild_service_role" {
-#   name        = "Codebuild_service_role"
-#   description = "codeBuild role to make changes on ECS"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "codepipeline.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-#   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser", ]
+resource "aws_iam_role" "codebuild_service_role" {
+  name        = "Codebuild_service_role"
+  description = "codeBuild role to make changes on ECS"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
+      },
+    ]
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser", ]
 
-# }
+}
+#codebuild service role policy
 
+resource "aws_iam_role_policy" "codebuild_policy" {
+  role = aws_iam_role.codebuild_service_role.id
+  name = "codebuild_policy_pipeline"
+  policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Effect = "Allow",
+          Resource = [
+            "arn:aws:logs:us-east-1:${var.account_number}:log-group:${var.codeBuild_loggroup}",
+            "arn:aws:logs:us-east-1:${var.account_number}:log-group:${var.codeBuild_loggroup}:*"
+          ],
+          Action = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ]
+        },
+        {
+          Effect = "Allow",
+          Resource = [
+            "arn:aws:s3:::${var.bucket_name}*"
+          ],
+          Action = [
+            "s3:PutObject",
+            "s3:GetObject",
+            "s3:GetObjectVersion",
+            "s3:GetBucketAcl",
+            "s3:GetBucketLocation"
+          ]
+        },
+        {
+          Effect = "Allow",
+          Resource = [
+            "arn:aws:codecommit:us-east-1:${var.account_number}:${var.source_repo_name}"
+          ],
+          Action = [
+            "codecommit:GitPull"
+          ]
+        },
+        {
+          Effect = "Allow",
+          Action = [
+            "codebuild:CreateReportGroup",
+            "codebuild:CreateReport",
+            "codebuild:UpdateReport",
+            "codebuild:BatchPutTestCases",
+            "codebuild:BatchPutCodeCoverages"
+          ],
+          Resource = [
+            "arn:aws:codebuild:us-east-1:${var.account_number}:report-group/*"
+          ]
+        },
+        {
 
-#pipeline artifact bucket 
-resource "aws_s3_bucket" "codePipeline_bucket" {
-  bucket = "codepipeline-gsingh"
+          Effect = "Allow",
+          Action = [
+            "secretsmanager:GetSecretValue"
+          ],
+          Resource = [
+            "${data.aws_secretsmanager_secret.name.arn}"
+          ]
+        }
+      ]
+    }
+  )
 }
 
+#docker's credentials store secre manager
+
+data "aws_secretsmanager_secret" "name" {
+  name = var.secret_manager_name
+}
+
+#codebuild project for pipeline
+
+resource "aws_codebuild_project" "codebuild_project" {
+  name = "ecs_codebuild"
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    environment_variable {
+
+      name  = "DOCKERHUB_USERNAME"
+      type  = "SECRETS_MANAGER"
+      value = "dockerhub:username"
+    }
+    environment_variable {
+
+      name  = "DOCKERHUB_PASSWORD"
+      type  = "SECRETS_MANAGER"
+      value = "dockerhub:password"
+    }
+    image           = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    privileged_mode = true
+    type            = "LINUX_CONTAINER"
+  }
+
+  source {
+    type = "CODECOMMIT"
+    git_submodules_config {
+      fetch_submodules = true
+    }
+    location = "https://git-codecommit.us-east-1.amazonaws.com/v1/repos/${var.source_repo_name}"
+  }
+  build_timeout = 5
+  description   = "project for ecs pipeline"
+  service_role  = aws_iam_role.codebuild_service_role.arn
+  logs_config {
+    cloudwatch_logs {
+      group_name  = var.codeBuild_loggroup
+      status      = "ENABLED"
+      stream_name = var.codeBuild_streamgroup
+
+    }
+  }
+
+}
+#pipeline artifact bucket 
+resource "aws_s3_bucket" "codePipeline_bucket" {
+  bucket = var.bucket_name
+}
 
 #create pipeline 
 
-
 resource "aws_codepipeline" "pipeline" {
-  name     = "ecs_pipeline"
+  name     = var.ecr_repo_name
   role_arn = aws_iam_role.codepipeline_service_role.arn
   artifact_store {
     location = aws_s3_bucket.codePipeline_bucket.bucket
@@ -383,14 +493,14 @@ resource "aws_codepipeline" "pipeline" {
   stage {
     name = "codecommit_source"
     action {
-      name             = "source_codeCommit"
+      name             = "Source"
       category         = "Source"
       owner            = "AWS"
       provider         = "CodeCommit"
       version          = "1"
       output_artifacts = ["source_output"]
       configuration = {
-        RepositoryName       = var.repo_name
+        RepositoryName       = var.source_repo_name
         BranchName           = "main"
         PollForSourceChanges = true
       }
@@ -399,7 +509,7 @@ resource "aws_codepipeline" "pipeline" {
   stage {
     name = "build"
     action {
-      name             = "ecs_pipeline"
+      name             = "Build"
       category         = "Build"
       owner            = "AWS"
       provider         = "CodeBuild"
@@ -407,14 +517,14 @@ resource "aws_codepipeline" "pipeline" {
       input_artifacts  = ["source_output"]
       output_artifacts = ["build_output"]
       configuration = {
-        ProjectName = "ecs_build_pipeline"
-        
+        ProjectName = aws_codebuild_project.codebuild_project.id
+
       }
-      #role_arn = aws_iam_role.codebuild_service_role.arn
+
     }
   }
   stage {
-    name = "deploy"
+    name = "Deploy"
     action {
       name            = "Deploy"
       category        = "Deploy"
@@ -423,7 +533,7 @@ resource "aws_codepipeline" "pipeline" {
       version         = "1"
       input_artifacts = ["build_output"]
       configuration = {
-        ClusterName = aws_ecs_cluster.cluster.id
+        ClusterName = var.cluster_name
         ServiceName = aws_ecs_service.pipeline_service.name
 
       }
