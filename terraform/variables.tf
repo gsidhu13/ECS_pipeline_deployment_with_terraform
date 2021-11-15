f3bfdf9724d1f5834898345fcb844d511a947b53
Gurlal <ggssidhu12@gmail.com>
2021-11-15 01:49:43 -0800

    update variable.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,41 @@
-variable "repo_name" {
-    type = string 
-    description = "declaring codeCommit Repo name"
-    default = "pipeline_source_repo"
-  
+variable "source_repo_name" {
+  type        = string
+  description = "declaring codeCommit Repo name"
+  default     = "pipeline_source_repo"
+}
+variable "account_number" {
+  type    = number
+  default = 460007636749
+
+}
+variable "cluster_name" {
+  type    = string
+  default = "ecs_cluster"
+
+}
+variable "bucket_name" {
+  type    = string
+  default = "codepipeline-gsingh"
+
+}
+#will need for buildspec file 
+variable "ecr_repo_name" {
+  type    = string
+  default = "ecs_pipeline"
+
+}
+variable "codeBuild_loggroup" {
+  type    = string
+  default = "codebuild_ecs_group"
+
+}
+variable "codeBuild_streamgroup" {
+  type    = string
+  default = "codebuild_ecs_stream"
+}
+
+variable "secret_manager_name" {
+  type    = string
+  default = "dockerhub"
+
 }
